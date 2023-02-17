c3cb6a2850ae07df10219438bddef92fe00f57ac
Ayush Bhardwaj <96904351+ay-bh@users.noreply.github.com>
2023-02-17 23:59:36 +0530

    Added Countdown

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Unix timestamp (in seconds) to count down to
+
+  const targetDate = Date.parse('Mar 24, 2023 00:00:00') / 1000;
+
+  // Set up FlipDown
+  var flipdown = new FlipDown(targetDate,{
+    theme: "light",
+  })
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      console.log('The countdown has ended!');
+    });
+
+  // Toggle theme
+  
+  
+  var ver = document.getElementById('ver');
+  ver.innerHTML = flipdown.version;
+});
